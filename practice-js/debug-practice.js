75dbd0c1c52d78deb403117ca2172146743d1a6f
G384292023 <238429@st.takushoku-u.ac.jp>
2024-05-18 12:15:19 +0900

    コミット

--- practice-js/debug-practice.js
+++ practice-js/debug-practice.js
@@ -1,15 +1,15 @@
 let heikin = 3.5;
-console.log(Heikin);        // 正しくは heikin
+console.log(heikin);        // 正しくは heikin
 
 // (2) 関数名のつづり間違い      → エラー発生
-let h = Math.froor(heikin); // 正しくは floor
+let h = Math.floor(heikin); // 正しくは floor
 
 // (3) 存在しない配列要素       → undefined　（エラーにならない）
 let ary = ['a', 'b', 'c'];
 console.log(ary[3]);        // 3番目の要素は存在しない
 
 // (4) 小数のインデックス       → undefined　（エラーにならない）
-let i = 3/2;
+let i = Marh.floor(3/2);
 console.log(ary[i]);        // i=3/2=1.5 なので ary[1.5] を参照しようとする
 
 // (5) メンバー名の間違い       → undefined （エラーにならない）
@@ -17,7 +17,7 @@ let obj = {x:3, y:7};
 console.log(obj.z);         // メンバー z は存在しない
 
 // (6) 初期化していない変数のメンバー   → エラー発生
-let o;
+let o = 6;
 console.log(o.x);           // oは初期化していない
 
 // (7) オブジェクトや配列ではない値のメンバーと要素	→ undefined （エラーにならない）
